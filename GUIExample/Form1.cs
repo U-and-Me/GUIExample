0126681c4f39b5ff53be2c5fe142cb7271860589
U-and-Me <yunasun0920@gmail.com>
2022-09-14 10:08:10 +0900

    레이블, 링크 레이블, 체크박스 UI 실습

--- GUIExample/Form1.cs
+++ GUIExample/Form1.cs
@@ -15,11 +15,85 @@ namespace GUIExample
         public Form1()
         {
             InitializeComponent();
+
+            // 레이블 생성
+            Label label = new Label()
+            {
+                Text = "글자",
+                Location = new Point(10, 110)
+            };
+
+            Controls.Add(label);
+
+            // 링크 레이블 생성
+            LinkLabel linkLabel = new LinkLabel()
+            {
+                //Text = "네이버",
+                Text = "메모장",
+                Location = new Point(10, 150)
+            };
+
+            // 요소에 이벤트 연결
+            linkLabel.Click += LabelClick;
+
+            // 화면에 요소 추가
+            Controls.Add(linkLabel);
+
+            // 체크 박스 생성
+            CheckBox checkBox1 = new CheckBox();
+            CheckBox checkBox2 = new CheckBox();
+            CheckBox checkBox3 = new CheckBox();
+            Button button = new Button();
+
+            // 요소 속성 설정
+            checkBox1.Text = "감자";
+            checkBox2.Text = "고구마";
+            checkBox3.Text = "토마토";
+            button.Text = "클릭";
+
+            checkBox1.Location = new Point(120, 100);
+            checkBox2.Location = new Point(120, 140);
+            checkBox3.Location = new Point(120, 170);
+            button.Location = new Point(120, 200);
+
+            // 클릭 이벤트
+            button.Click += ButtonClick;
+
+            Controls.Add(checkBox1);
+            Controls.Add(checkBox2);
+            Controls.Add(checkBox3);
+            Controls.Add(button);
+        }
+
+        private void ButtonClick(object sender, EventArgs e)
+        {
+            List<String> list = new List<string>();
+
+            foreach(var item in Controls)
+            {
+                if (item is CheckBox)
+                {
+                    CheckBox checkBox = (CheckBox) item;
+
+                    if (checkBox.Checked)
+                    {
+                        list.Add(checkBox.Text);
+                    }
+                }
+            }
+
+            MessageBox.Show(string.Join(",", list));
+        }
+
+        private void LabelClick(object sender, EventArgs e)
+        {
+            //System.Diagnostics.Process.Start("https://naver.com");
+            System.Diagnostics.Process.Start("notepad.exe");
         }
 
         private void 메모장정보ToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            
+            MessageBox.Show("메모장 정보");
         }
 
         int STEP = 20;
